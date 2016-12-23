aa04a4ce6ddb7ef8a78c7ab3ee4de0f528613954
PineChang <1034638186@qq.com>
2016-12-23 19:07:44 +0800

    提交项目架构

---
+++ ZPBudejie/ZPBudejie/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZPBudejie
+//
+//  Created by 管理员 on 2016/12/23.
+//  Copyright © 2016年 com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ZPBudejie/ZPBudejie/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZPBudejie
+//
+//  Created by 管理员 on 2016/12/23.
+//  Copyright © 2016年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
