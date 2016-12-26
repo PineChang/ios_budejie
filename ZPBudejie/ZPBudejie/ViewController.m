e7c7e85f30c07d56f6ec666f0b58fc26af19e5f4
PineChang <1034638186@qq.com>
2016-12-26 16:55:41 +0800

    提交第一次修改

--- ZPBudejie/ZPBudejie/ViewController.m
+++ ZPBudejie/ZPBudejie/ViewController.m
@@ -15,12 +15,14 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+	//只加载一次,如果加载比较多,用viewWillAppear
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+    //电池没有电的时候回调的方法;
     // Dispose of any resources that can be recreated.
 }
 
